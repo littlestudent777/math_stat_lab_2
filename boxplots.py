4c05de631e79ab4a632ee5515277c2542da62fd5
Alexandra_B <balakshina.ad@edu.spbstu.ru>
2025-03-04 23:35:12 +0300

    Added annotation

--- boxplots.py
+++ boxplots.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 
 
-def build_boxplots(ns, distributions):
+def build_boxplots(sample_size: list[int], distributions: list) -> None:
 
     save_path = "boxplots"
     os.makedirs(save_path, exist_ok=True)  # Create a directory for graphs, if it doesn't exist
@@ -12,7 +12,7 @@ def build_boxplots(ns, distributions):
         figure, axes = plt.subplots(3, 1, figsize=(20, 10))
         figure.suptitle(f'{distribution_name} distribution', fontsize=18)
 
-        for index, n in enumerate(ns):
+        for index, n in enumerate(sample_size):
             values = distribution_f(n)
 
             sns.boxplot(values, ax=axes[index], orient='h', color='skyblue',
--- outlier.py
+++ outlier.py
@@ -2,7 +2,8 @@ import numpy as np
 import pandas as pd
 import jinja2
 
-def count_outliers(data):
+
+def count_outliers(data: list) -> int:
     Q1 = np.quantile(data, 0.25)
     Q3 = np.quantile(data, 0.75)
     IQR = Q3 - Q1
@@ -12,12 +13,12 @@ def count_outliers(data):
     return len(outliers)
 
 
-def print_outliers_table(ns, distributions):
+def print_outliers_table(sample_size: list[int], distributions: list) -> None:
     data = []
 
     # Fill data list
     for distribution_name, distribution_func in distributions:
-        for n in ns:
+        for n in sample_size:
             values = distribution_func(n)
             outliers_count = count_outliers(values)
 
@@ -36,7 +37,8 @@ def print_outliers_table(ns, distributions):
         table_data = group[['Sample size', 'Outliers']]
 
         # Generate LaTeX table
-        latex_table = table_data.to_latex(index=False, caption=f"{distribution_name} distribution", label=f"tab:{distribution_name}")
+        latex_table = table_data.to_latex(index=False, caption=f"{distribution_name} distribution",
+                                          label=f"tab:{distribution_name}")
         latex_table = latex_table.replace('\\begin{table}', '\\begin{table}[H] \\centering')
         latex_table = latex_table.replace('\\caption{', '\\caption*{')
         print(latex_table)
